810e1170548c9d7fee52b47ced8db21a70728c9a
Rustam <rustam.mustafoev01@gmail.com>
2023-05-13 06:33:21 +0500

    feat: project models schema

--- core/models.py
+++ core/models.py
@@ -1,3 +1,125 @@
 from django.db import models
 
-# Create your models here.
+from user.models import Doctor, Patient
+
+
+class WeekDay(models.TextChoices):
+    MONDAY = 'MON', 'Monday'
+    TUESDAY = 'TUE', 'Tuesday'
+    WEDNESDAY = 'WED', 'Wednesday'
+    THURSDAY = 'THU', 'Thursday'
+    FRIDAY = 'FRI', 'Friday'
+    SATURDAY = 'SAT', 'Saturday'
+    SUNDAY = 'SUN', 'Sunday'
+
+
+class CurrencyType(models.TextChoices):
+    USD = 'USD', 'US Dollars'
+    UZB = 'UZB', 'Uzbek Soums',
+    EUR = 'EUR', 'Euro'
+
+
+class PaymentType(models.TextChoices):
+    CASH = 'CASH', 'Via Cash'
+    CARD = 'CARD', 'Via Card'
+
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'appointment'
+
+
+class Schedule(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    day = models.CharField(max_length=3, choices=WeekDay)
+
+    class Meta:
+        db_table = 'schedule'
+
+
+class Prescription(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.DO_NOTHING)
+    doctor = models.ForeignKey(Doctor, on_delete=models.DO_NOTHING)
+    medicines = models.ManyToManyField('Medicine', related_name='medicines')
+    tests = models.ManyToManyField('MedicalTest', related_name='tests')
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'prescription'
+
+
+class PrescriptionComment(models.Model):
+    prescription = models.OneToOneField(Prescription, on_delete=models.DO_NOTHING)
+    comment = models.TextField()
+
+    class Meta:
+        db_table = 'prescription_comment'
+
+
+class Payment(models.Model):
+    payment_type = models.CharField(max_length=4, choices=PaymentType, default=PaymentType.CASH.value)
+    patient = models.ForeignKey(Patient, on_delete=models.DO_NOTHING)
+    amount = models.PositiveIntegerField()
+    currency_type = models.CharField(max_length=3, choices=CurrencyType, default=CurrencyType.USD.value)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'payment'
+
+
+class MedicalTest(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'medical_test'
+
+
+class Notification(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.DO_NOTHING)
+    patient = models.ForeignKey(Patient, on_delete=models.DO_NOTHING)
+    message_content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'notification'
+
+
+class Medicine(models.Model):
+    name = models.CharField(max_length=100)
+    group = models.ForeignKey('MedicineGroup', on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'medicine'
+
+
+class MedicineGroup(models.Model):
+    title = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'medicine_group'
+
+
+class HospitalInventory(models.Model):
+    medicine = models.OneToOneField(Medicine, on_delete=models.DO_NOTHING, related_name='medicine')
+    supplier = models.ForeignKey('Pharmacy', on_delete=models.DO_NOTHING, related_name='medicine_supplier')
+    unit_price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
+
+    class Meta:
+        db_table = 'hospital_inventory'
+
+
+class Pharmacy(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'pharmacy'
--- user/models.py
+++ user/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class HospitalAdmin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    year_of_experience = models.PositiveIntegerField()
+    phone = models.CharField(max_length=50)
+    department = models.ForeignKey('Department', on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'doctor'
+
+
+class Nurse(models.Model):
+    name = models.CharField(max_length=100)
+    year_of_experience = models.PositiveIntegerField()
+    phone = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'nurse'
+
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'patient'
+
+
+class Department(models.Model):
+    title = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'hospital_department'
+
+    @property
+    def total_employees_amount(self):
+        pass
